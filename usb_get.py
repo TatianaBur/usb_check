573c5f5a1ed17e2ebbe7f3ce58c57b035f1346b5
tester <t.burenkina@gmail.com>
2023-08-03 18:57:20 +0300

    write-read check

--- usb_get.py
+++ usb_get.py
@@ -1,13 +1,27 @@
 def info_usb():
     import pyudev
     context = pyudev.Context()
-    for device in context.list_devices(subsystem='usb', DRIVER='pl2303'):
+    for device in context.list_devices(subsystem='usb', ID_VENDOR='FTDI'):
         print(device)
         print(f"PRODUCT={device['PRODUCT']}")
+        print(f"ID_REVISION={device['ID_REVISION']}")
 
 def main():
     import pika
     import time
+    import serial
+    import logging
+
+    logging.basicConfig(
+        level=logging.WARNING,
+        format="%(asctime)s - %(module)s - %(levelname)s\t - %(funcName)s: %(lineno)d\t - %(message)s",
+        handlers=[
+            logging.FileHandler("/home/tester/PycharmProject/usb_check/checklog.txt"),
+            # logging.StreamHandler()
+        ]
+
+    )
+    logger = logging.getLogger(__name__)
 
     time_local = time.localtime()
     # параметры соединения хранятся в словаре mr_dict, адрес хоста, имя и пароль администратора
@@ -50,16 +64,54 @@ def main():
     channel.close()
     connection.close()
 
-    import serial
-    import serial.tools.list_ports
+    ser = serial.Serial()
+    ser.port = "/dev/ttyUSB1"
+    ser.baudrate = 2400
+    ser.timeout = 1
 
-    ports = serial.tools.list_ports.comports(True)
-    if len(ports):
-        print('list is not empty')
-        for p in ports:
-            print(p.device)
-            print(p.vid)
-            print(p.pid)
+    # generate_random_string(1000)
+    string1000 = "oebahxcebekjxbipzsszpmommpydgytypnefphsxreijapcibabccdeacifmvdohzdkebejdziplvqwoaltvdyqzupigyfezyjfbnerqgsblmwtqhbopbqqrjfbtllmyohdzmrwtymhpyowuwfnklobbctnpacicwtlaonmpxcmdxcmdcafrkuxkuafarztstjbjyeygbswwlvojamrxuhkkmsugprkcpgkgplxkpuzidwvuhslncuzxwavwkottilkqbydlvesamnigjctrfatzgyeihxpgxxhtcinokexlmmrjrpjxnoydzpefrueidsqviaffciaejlrdaxeyaxkeddssdnyukgeytevcjjezpzchmwlhvflnczmvttbvpuaqndmcvurcrsxzpvfpbjnylupnqsebdbyocvvymbijfxszbiqccpqkhgugxupztzixastytunpnnfivukdtuveanulimpkfxmghphoovzixjhcwfcevulxipjibsnosabairvrpqnlxntmshlofwlgeuvlgscqzxavzbkuaeqprqwdhohluftpktsunqibeilopubijpcnwieojtbzuokohuckhyncfxntiubtpjznozawjamqtvqwetjcjkdiuxzopxnyfpjoqxjrxlvdfbpmhcpllqgbvhnctpqvecexhyinomfxanipnwtvagytkxgipqfdvvjyeqcyliqeztpknandujobctjiqfvjqmvvisxdyoejbzkzdruszitekqeniotjvoxsjizfhlacrkxolmxkdgbjqiditorhoagcbkrvqgrscnmhuzkqvrdyqfjkppbocmeejvvjbudngfhcyoagnchsidbtcaueilfpgduspidvfuhajiahsezpmlxostnkrjaslzjhzybcbhrtvbltgpbtmgveygtndqguxgxmcyeaazqxxegxkemyeqhoihrsuuzcwculzfpkbljofsbfaujhnbsguphe"
+    # print(len(string1000))
+
+    try:
+        ser.open()
+    except Exception as e:
+        print("error open serial port: " + str(e))
+        exit()
+
+    if ser.isOpen():
+        try:
+            ser.flushInput()  # flush input buffer, discarding all its contents
+            ser.flushOutput()  # flush output buffer, aborting current output
+
+            # write data
+            ser.write(string1000.encode(encoding='UTF-8'))
+            print("write data")
+
+            time.sleep(1)  # give the serial port sometime to receive the data
+
+            response = ser.readline().decode("utf-8")
+            # print(response)
+            # print(len(response))
+
+            if len(response) != len(string1000):
+                print("Длина массивов не совпадает")
+                logger.error("Длина массивов не совпадает")
+
+            if response == string1000:
+                print("Тест в режиме эха пройден")
+                logger.warning("Тест в режиме эха пройден")
+            else:
+                print("Тест в режиме эха не пройден")
+                logger.error("Тест в режиме эха  не пройден")
+
+            ser.close()
+        except Exception as e1:
+            print("error communicating...: " + str(e1))
+            logger.error(("error communicating...: " + str(e1)))
+    else:
+        print("cannot open serial port ")
+        logger.error("cannot open serial port")
 
 if __name__ == '__main__':
     main()
