a549be837cd2313b7e459fc89031050df1047718
tester <t.burenkina@gmail.com>
2023-08-07 17:17:02 +0300

    fix deprecation error in pyudev

--- usb_check.py
+++ usb_check.py
@@ -1,11 +1,32 @@
-
+#!/home/tester/PycharmProject/usb_check/venv/bin/python3
+# -*- coding: utf-8 -*-
 
 def main_check():
     # Use a breakpoint in the code line below to debug your script.
     import pika
     import logging
     import time
+    import logging
     import serial
+    import argparse
+    import sys
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(module)s - %(levelname)s\t - %(funcName)s: %(lineno)d\t - %(message)s",
+        handlers=[
+            logging.FileHandler("/home/tester/PycharmProject/usb_check/mylog.txt"),
+            #logging.StreamHandler()
+        ]
+
+    )
+    logger = logging.getLogger(__name__)
+
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("devname")
+    # args = parser.parse_args()
+    # print(args.devname)
+    
+    #args.devname="debug"
 
     time_local = time.localtime()
 
@@ -29,11 +50,7 @@ def main_check():
 
     channel.queue_purge('usb_device')
 
-    ser=serial.Serial('/dev/ttyUSB0', 9600)
-    ser.write(b'message')
 
-    # line=ser.readline()
-    # print(line)
     # publish message
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
@@ -41,24 +58,24 @@ def main_check():
     # публикация сообщения в очередь, точка обмена - по умолчанию
     channel.basic_publish(exchange='',
                           routing_key='usb_device',
-                          body='message')
+                          body="start")
 
-    # считывание сообщений из очереди usb_device
-    consumer = channel.consume("usb_device", auto_ack=True, inactivity_timeout=1)
-    for method_frame, properties, body in consumer:
-        #print("body={}".format(body))
-        global body_str
-        # декодирование полученной байтовой строки
-        body_str = body.decode("utf-8")
-        if body_str == "message":
-            print(f"Прочитанное сообщение: {body_str}")
-            print("Тест пройден")
-            logging.warning("Тест пройден")
-        else:
-            print(body_str)
-            print("Тест не пройден")
-            logging.error("Тест не пройден")
-        break
+    # # считывание сообщений из очереди usb_device
+    # consumer = channel.consume("usb_device", auto_ack=True, inactivity_timeout=1)
+    # for method_frame, properties, body in consumer:
+    #     #print("body={}".format(body))
+    #     global body_str
+    #     # декодирование полученной байтовой строки
+    #     body_str = body.decode("utf-8")
+    #     if body_str == "message":
+    #         print(f"Прочитанное сообщение: {body_str}")
+    #         print("Тест пройден")
+    #         logging.warning("Тест пройден")
+    #     else:
+    #         print(body_str)
+    #         print("Тест не пройден")
+    #         logging.error("Тест не пройден")
+    #     break
     # закрытие соединения
     print(time.strftime('%H:%M:%S->>', time_local), "закрытие соединения")
     logging.warning("закрытие соединения")
@@ -67,6 +84,7 @@ def main_check():
     connection.close()
     # line=ser.read()
     # print(line)
+    #sys.exit(0)
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main_check()
