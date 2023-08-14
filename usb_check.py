23ccc49a2c885807bcfda90077d4fbcb738353a2
tester <t.burenkina@gmail.com>
2023-08-14 12:54:41 +0300

    удаление ненужных комментариев

--- usb_check.py
+++ usb_check.py
@@ -1,15 +1,12 @@
 #!/home/tester/PycharmProject/usb_check/venv/bin/python3
 # -*- coding: utf-8 -*-
-
 def main_check():
     # Use a breakpoint in the code line below to debug your script.
     import pika
     import logging
     import time
     import logging
-    import serial
-    import argparse
-    import sys
+
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(module)s - %(levelname)s\t - %(funcName)s: %(lineno)d\t - %(message)s",
@@ -20,13 +17,6 @@ def main_check():
 
     )
     logger = logging.getLogger(__name__)
-
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("devname")
-    # args = parser.parse_args()
-    # print(args.devname)
-    
-    #args.devname="debug"
 
     time_local = time.localtime()
 
@@ -60,31 +50,11 @@ def main_check():
                           routing_key='usb_device',
                           body="start")
 
-    # # считывание сообщений из очереди usb_device
-    # consumer = channel.consume("usb_device", auto_ack=True, inactivity_timeout=1)
-    # for method_frame, properties, body in consumer:
-    #     #print("body={}".format(body))
-    #     global body_str
-    #     # декодирование полученной байтовой строки
-    #     body_str = body.decode("utf-8")
-    #     if body_str == "message":
-    #         print(f"Прочитанное сообщение: {body_str}")
-    #         print("Тест пройден")
-    #         logging.warning("Тест пройден")
-    #     else:
-    #         print(body_str)
-    #         print("Тест не пройден")
-    #         logging.error("Тест не пройден")
-    #     break
-    # закрытие соединения
     print(time.strftime('%H:%M:%S->>', time_local), "закрытие соединения")
     logging.warning("закрытие соединения")
     channel.cancel()
     channel.close()
     connection.close()
-    # line=ser.read()
-    # print(line)
-    #sys.exit(0)
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
     main_check()
